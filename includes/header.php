defb8ee7cfec7a2c3b1a56bf46bf7cd7cfced90f
Marcos Alexandre <marcos.alex1002@gmail.com>
2024-11-29 09:35:27 -0300

    🛠️ chore(includes): Atualizado links para ter uma BASE_URL

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
+<?php
+// Define o caminho base do site
+define('BASE_URL', '/GitHub/FinanX/'); // Substitua '/meu-projeto/' pelo caminho correto
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -9,7 +13,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/header.css">
+    <!-- <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/header.css"> -->
 
     <title>FinanX</title>
 </head>
@@ -17,7 +21,7 @@
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="/">FinanX</a>
+            <a class="navbar-brand" href="<?= BASE_URL ?>">FinanX</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
                 aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
                 <span class="navbar-toggler-icon"></span>
@@ -26,10 +30,10 @@
             <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="./">Painel <span class="sr-only">(página atual)</span></a>
+                        <a class="nav-link" href="<?= BASE_URL ?>index.php">Painel</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="pages/sistema/transacao.php">Transação</a>
+                        <a class="nav-link" href="<?= BASE_URL ?>pages/sistema/transacao.php">Transação</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -37,8 +41,8 @@
                             Cadastro
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="pages/cadastro/categoria.php">Categoria</a>
-                            <a class="dropdown-item" href="pages/cadastro/subcategoria.php">Subcategoria</a>
+                            <a class="dropdown-item" href="<?= BASE_URL ?>pages/cadastro/categoria.php">Categoria</a>
+                            <a class="dropdown-item" href="<?= BASE_URL ?>pages/cadastro/subcategoria.php">Subcategoria</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Algo mais aqui</a>
                         </div>
--- pages/sistema/transacao.php
+++ pages/sistema/transacao.php
@@ -54,63 +54,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
+include '../../includes/header.php'
+
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-    <!-- Meta tags Obrigatórias -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <title>FinanX</title>
-</head>
-
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="../index.html">FinanX</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
-                aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="../_Sistema/transacao.php">link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Cadastro
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="../_Cadastros/categoria.php">Categoria <span class="sr-only">(página atual)</span></a>
-                            <a class="dropdown-item" href="../_Cadastros/subcategoria.php">Subcategoria</a>
-                            <a class="dropdown-item" href="#">Outra ação</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Algo mais aqui</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <a class="nav-link disabled" href="#">Desativado</a>
-                </form>
-            </div>
-        </nav>
-    </header>
 
     <div class="container">
         <br>
