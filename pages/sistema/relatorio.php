7283e8ef87ab3ebbe01abbc50d1e3de80f63b3f9
Marcos Alexandre <marcos.alex1002@gmail.com>
2025-01-14 17:29:05 -0300

    ✨ feat(relatorio): simplificar estrutura HTML e incluir cabeçalho

--- pages/sistema/relatorio.php
+++ pages/sistema/relatorio.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Relatório</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+<?php
+include '../../includes/header.php'
+?>
     <!-- Container Principal -->
     <div class="container my-4">
         <!-- Cabeçalho -->
