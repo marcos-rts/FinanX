d2dd5ba8c54c95d724f23f1ea12d5fa6a02d062a
Marcos Alexandre <marcos.alex1002@gmail.com>
2025-01-14 09:58:35 -0300

    ✨ feat(database): adicionar timestamps padrão nas tabelas para rastreamento de criação e edição

--- config/database/bd_notificacoes.sql
+++ config/database/bd_notificacoes.sql
@@ -8,16 +8,16 @@ CREATE TABLE
         enviada BOOLEAN DEFAULT FALSE,
         FOREIGN KEY (transacao_id) REFERENCES sis_transacao (id),
         -- Bloco padrão 
-        criado_em TIMESTAMP, -- Data da Criação
+        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data da Criação
         criado_por INT, -- Usuario que criou
-        editado_em TIMESTAMP, -- Data da edição
+        editado_em TIMESTAMP NULL DEFAULT NULL, -- Data da edição
         editado_por INT, -- Usuario que editou
-        excluido_em TIMESTAMP, -- Data da exclusão
+        excluido_em TIMESTAMP NULL DEFAULT NULL, -- Data da exclusão
         excluido_por INT, -- Usuario que excluiu
-        ativado_em TIMESTAMP, -- Data da ativação
+        ativado_em TIMESTAMP NULL DEFAULT NULL, -- Data da ativação
         ativado_por INT, -- Usuario que ativou
-        inativado_em TIMESTAMP, -- Data da inativação
+        inativado_em TIMESTAMP NULL DEFAULT NULL, -- Data da inativação
         inativado_por INT, -- Usuario que inativou
-        ativo BOOLEAN DEFAULT 1 -- Status do registro
+        ativo BOOLEAN DEFAULT 1, -- Status do registro
         excluido BOOLEAN DEFAULT 1 -- Status do registro
     );
--- config/database/sis_transacao.sql
+++ config/database/sis_transacao.sql
@@ -1,23 +1,6 @@
 -- Tabela de Transações
-<<<<<<< HEAD
-CREATE TABLE sis_transacao (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    valor DECIMAL(10, 2) NOT NULL,
-    mes INT NOT NULL,
-    ano INT NOT NULL,
-    tipo_id INT NOT NULL,
-    categoria_subcategoria_id INT NOT NULL,
-    pagamento_conta_id INT,
-    usuario_id INT NOT NULL,
-    evento_id INT, -- Opcional: Vincular transação a um evento
-    status ENUM('Pendente', 'Concluída', 'Cancelada') DEFAULT 'Concluída',
-    FOREIGN KEY (tipo_id) REFERENCES bd_tipo(id),
-    FOREIGN KEY (categoria_subcategoria_id) REFERENCES rlc_categoria_subcategoria(id),
-    FOREIGN KEY (pagamento_conta_id) REFERENCES rlc_pagamento_conta(id),
-    FOREIGN KEY (usuario_id) REFERENCES bd_usuario(id),
-    FOREIGN KEY (evento_id) REFERENCES bd_eventos(id)
-);
-=======
+
+
 CREATE TABLE
     sis_transacao (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,17 +19,17 @@ CREATE TABLE
         FOREIGN KEY (usuario_id) REFERENCES bd_usuario (id),
         FOREIGN KEY (evento_id) REFERENCES bd_eventos (id),
         -- Bloco padrão 
-        criado_em TIMESTAMP, -- Data da Criação
+        criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Data da Criação
         criado_por INT, -- Usuario que criou
-        editado_em TIMESTAMP, -- Data da edição
+        editado_em TIMESTAMP NULL DEFAULT NULL, -- Data da edição
         editado_por INT, -- Usuario que editou
-        excluido_em TIMESTAMP, -- Data da exclusão
+        excluido_em TIMESTAMP NULL DEFAULT NULL, -- Data da exclusão
         excluido_por INT, -- Usuario que excluiu
-        ativado_em TIMESTAMP, -- Data da ativação
+        ativado_em TIMESTAMP NULL DEFAULT NULL, -- Data da ativação
         ativado_por INT, -- Usuario que ativou
-        inativado_em TIMESTAMP, -- Data da inativação
+        inativado_em TIMESTAMP NULL DEFAULT NULL, -- Data da inativação
         inativado_por INT, -- Usuario que inativou
-        ativo BOOLEAN DEFAULT 1 -- Status do registro
+        ativo BOOLEAN DEFAULT 1, -- Status do registro
         excluido BOOLEAN DEFAULT 1 -- Status do registro
     );
->>>>>>> Dev_Database
+
